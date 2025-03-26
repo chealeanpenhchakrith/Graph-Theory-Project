6ff219ec4ec215ca643eaf86d3a35624441fc45a
chealeanpenhchakrith <leanpenhchakrith.chea@gmail.com>
2025-03-26 12:10:46 +0100

    Added a function which displays the graph as a form of triplets

--- function.py
+++ function.py
@@ -40,3 +40,16 @@ def edgeCounter(dataStored):
             edgeCounter += 1
             j += 1
     return edgeCounter
+
+#Define a function that displays the graph as a form of triplets
+def graphInFormOfTripets(updatedList):
+    iteration1 = len(updatedList)
+    for i in range (iteration1):
+        iteration2 = len(updatedList[i])
+        for j in range (iteration2):
+            if (j == 2 and j < iteration2 and updatedList[i][j] == '0'):
+                print(f"{updatedList[i][j]} -> {updatedList[i][0]} = 0")
+            else:
+                if (j >= 2 and j < iteration2 and updatedList[i][j] != '0'):
+                    print(f"{updatedList[i][j]} -> {updatedList[i][0]} = {updatedList[i][1]}")
+                    
